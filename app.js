db8e77963f56746ff93ddd8bc5d5e8f3803673ba
clandau <so.super@gmail.com>
2018-02-15 17:49:22 -0500

    more set up

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
 
+
 const cors = require('cors')
 app.use(cors({optionsSuccessStatus: 200}))
+
+const bodyParser = require('body-parser')
 
 app.use(express.static('public'))
 
 app.get(__dirname + '/views/index.html')
 
 
-
+// console.log(MONGO_URI)
 
 const listener = app.listen(PORT, () => {
     console.log('You are listening on port ' + PORT)
