85a14b4c2c01d10ed6a0f632fbccc3dc4a3da0d5
clandau <so.super@gmail.com>
2018-02-16 12:41:05 -0500

    add html and styling

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 const cors = require('cors')
 app.use(cors({optionsSuccessStatus: 200}))
@@ -11,9 +18,6 @@ const bodyParser = require('body-parser')
 app.use(express.static('public'))
 
 app.get(__dirname + '/views/index.html')
-
-
-// console.log(MONGO_URI)
 
 const listener = app.listen(PORT, () => {
     console.log('You are listening on port ' + PORT)
