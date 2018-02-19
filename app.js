eaf69be1f1148271b2c346798b73d17dfa986807
clandau <so.super@gmail.com>
2018-02-19 15:35:14 -0500

    add form, database schema

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -9,16 +9,31 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
+const schema = mongoose.Schema;
 
-const cors = require('cors')
-app.use(cors({optionsSuccessStatus: 200}))
+const cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+const urlSchema = new schema({
+    original_url: {type: String, required: true},
+    short_url: {type: String, required: true}
+});
+const Url = mongoose.model('Url', urlSchema);
+
+//parse url to get the url the user wishes to shorten
+app.get('/api/shorturl/new/:original_url?', (req, res) => {
+    //test to see if url is valid, if not, send error JSON
+    //if so, do the work
+
 })
 
 
