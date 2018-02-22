c51d38ac9caaac8e3ead07acb3557dc41432435c
clandau <so.super@gmail.com>
2018-02-22 18:26:18 -0500

    add function to create new database entry

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 8000;
 
 //to validate entered addresses
 const dns = require('dns');
 
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
@@ -23,7 +25,8 @@ let urlEncodedParser = bodyParser.urlencoded({ extended: false });
 //regex to test if the submitted url is valid
 const urlRegex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
 
-app.use(express.static('public'));
+//counter to use for shortened URL
+let count = 1001;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
@@ -35,12 +38,28 @@ const urlSchema = new schema({
 });
 const Url = mongoose.model('Url', urlSchema);
 
+//function to make sure that the address to shorten is valid
 let validAddress = ((addressToValidate) => {
     // console.log(addressToValidate);
     return urlRegex.test(addressToValidate) && dns.lookup(addressToValidate, (err, address, family) => {
         //console.log(address);
         return address ? true : false;
     })});
+
+//function to create a new database entry and short url
+let createNewShortUrl = function(longUrl, done) {
+    let newUrlHolder = new Url({original_url: longUrl, short_url: 'test'});
+    count++;
+    newUrlHolder.save((err, data) => {
+        if(err) done(err);
+        done(null, data);
+    });
+};
+
+//function to test createNewShortUrl function (works)
+createNewShortUrl('http://www.google.com', (err, data) => {
+    console.log(err ? err : data);
+});
 
 app.get('/api/shorturl/new/:url(*)', (req, res, next) => {
     let enteredUrl = req.params.url;
@@ -61,8 +80,11 @@ app.post('/api/shorturl/new/', urlEncodedParser, (req, res, next) => {
         res.send({ 'error' : "invalid URL" })
     }
     //if so, do the work
-    else res.send({'url': enteredUrl});
-    console.log('success');
+    else {
+        //create new url, post to DB, send to user
+        res.send({'url': enteredUrl});
+        console.log('success');
+    }
     next();
 })
 
