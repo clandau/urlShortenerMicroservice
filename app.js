45c82372b32940d0c26529c4be463de1c3283217
clandau <so.super@gmail.com>
2018-03-02 19:16:25 -0500

    change to database uri

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 const schema = mongoose.Schema;
